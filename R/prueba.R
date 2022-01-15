7f1d66e78ac75e7be198ecd85b81bf1cd3b15b71
Daniel IT <itd617@gmail.com>
2022-01-14 19:47:38 -0500

    first commit

---
+++ R/prueba.R
@@ -0,0 +1,12 @@
+## ----setup, include=FALSE-------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+
+## ----cars-----------------------------------------------------
+require(dplyr)
+x <- 5
+
+
+## ----pressure, echo=FALSE-------------------------------------
+plot(pressure)
+
